303df2b6c9467c076053e56d82e75e5ae0fa1040
janetli314 <31289607+janetli314@users.noreply.github.com>
2020-05-13 15:28:20 -0700

    Create Numpy.py

---
+++ Introduction to Python/Numpy.py
@@ -0,0 +1,5 @@
+/*
+Import the numpy package as np, so that you can refer to numpy with np.
+Use np.array() to create a numpy array from baseball. Name this array np_baseball.
+Print out the type of np_baseball to check that you got it right.
+*/
