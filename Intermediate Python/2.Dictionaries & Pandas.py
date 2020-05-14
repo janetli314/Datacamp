3ac41ed8f33aeb6e4319bfee16c593ddb6139988
janetli314 <31289607+janetli314@users.noreply.github.com>
2020-05-13 21:04:20 -0700

    Update 2.Dictionaries & Pandas.py

--- Intermediate Python/2.Dictionaries & Pandas.py
+++ Intermediate Python/2.Dictionaries & Pandas.py
@@ -227,12 +227,10 @@ print(cars.loc[['AUS','EG']])
 Print out the drives_right value of the row corresponding to Morocco (its row label is MOR)
 Print out a sub-DataFrame, containing the observations for Russia and Morocco and the columns country and drives_right
 '''
-# Import cars data
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
-
-# Print out drives_right value of Morocco
-print(cars.iloc[[5,2]])
+print(cars.iloc[5,2])
+print(cars.iloc[[4,5],[1,2]])
 
 '''
 Print out the drives_right column as a Series using loc or iloc.
